c4d02ec7460e3ab3f3230a28cb1bcd0359a2bc9b
Beatriz de Henriques Martins <up201502858@fe.up.pt>
2018-10-25 17:50:06 +0100

    Inicio do agente paciente

---
+++ Trabalho_1/src/Behaviour/PatientBehaviour.java
@@ -0,0 +1,35 @@
+package Behaviour;
+
+import Agents.PatientAgent;
+import jade.core.Agent;
+import jade.lang.acl.ACLMessage;
+import jade.proto.AchieveREInitiator;
+import java.util.Vector;
+
+public class PatientBehaviour extends AchieveREInitiator {
+    public PatientBehaviour(Agent a, ACLMessage msg) {
+        super(a, msg);
+    }
+
+    protected Vector<ACLMessage> prepareRequests(ACLMessage msg) {
+        Vector<ACLMessage> v = new Vector<ACLMessage>();
+        // ...
+        return v;
+    }
+
+    protected void handleAgree(ACLMessage agree) {
+        // ...
+    }
+
+    protected void handleRefuse(ACLMessage refuse) {
+        // ...
+    }
+
+    protected void handleInform(ACLMessage inform) {
+        // ...
+    }
+
+    protected void handleFailure(ACLMessage failure) {
+        // ...
+    }
+}
