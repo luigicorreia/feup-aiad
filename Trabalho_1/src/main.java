36a84a1c3f97669b6edcdf6ed7d0c31c78f6cb49
zepedrob16 <ze_pedro_borges@hotmail.com>
2018-10-25 17:02:56 +0100

    main mudado para criar jade

--- Trabalho_1/src/main.java
+++ Trabalho_1/src/main.java
@@ -1,8 +1,28 @@
+import jade.Boot;
+import jade.core.Agent;
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.wrapper.AgentContainer;
+
 import java.io.*;
 
 public class main {
     public static void main(String args[]){
-        System.out.println("Olá!");
-        System.out.println("Olá!");
+        Runtime rt = Runtime.instance();
+
+        String[] container = {
+          "-gui",
+          "-host 127.0.0.1",
+          "-container"
+        };
+
+        Boot.main(container);
+
+        Profile profileHospitals = new ProfileImpl();
+        Profile profilePatients = new ProfileImpl();
+
+        AgentContainer ccHospitals = rt.createAgentContainer(profileHospitals);
+        AgentContainer ccPatients = rt.createAgentContainer(profilePatients);
     }
 }
