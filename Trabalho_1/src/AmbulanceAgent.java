ae757a3cffc34e01f3ba0fa4299f939734056823
zepedrob16 <ze_pedro_borges@hotmail.com>
2018-11-01 18:55:17 +0000

    started hospital-ambulance communication

--- Trabalho_1/src/AmbulanceAgent.java
+++ Trabalho_1/src/AmbulanceAgent.java
@@ -1,9 +1,80 @@
+import jade.core.AID;
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
 import jade.lang.acl.ACLMessage;
-import jade.lang.acl.MessageTemplate;
+import jade.proto.ContractNetInitiator;
+
+import java.util.Vector;
 
 public class AmbulanceAgent extends Agent{
-    public void setup(){
+    private Agent myAgent;
+
+    public void setup() {
+        myAgent = this;
+
         addBehaviour(new AmbulanceBehaviour(this, new ACLMessage(ACLMessage.CFP)));
+
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("ambulance");
+        sd.setName(getLocalName());
+        dfd.addServices(sd);
+        try {
+            DFService.register(this, dfd);
+        } catch(FIPAException fe) {
+            fe.printStackTrace();
+        }
+    }
+
+    public class AmbulanceBehaviour extends ContractNetInitiator {
+        public AmbulanceBehaviour(Agent a, ACLMessage cfp) {
+            super(a, cfp);
+        }
+
+        protected Vector prepareCfps(ACLMessage cfp) {
+            Vector v = new Vector();
+
+            DFAgentDescription template = new DFAgentDescription();
+            ServiceDescription sd = new ServiceDescription();
+            sd.setType("hospital");
+            template.addServices(sd);
+
+
+            try {
+                DFAgentDescription[] result = DFService.search(myAgent, template);
+                for(int i=0; i<result.length; ++i) {
+                    cfp.addReceiver(new AID(result[i].getName().toString(), false));
+                }
+            } catch(FIPAException fe) {
+                fe.printStackTrace();
+            }
+
+
+            cfp.setContent("need a hospital");
+
+            v.add(cfp);
+
+            return v;
+        }
+
+        protected void handleAllResponses(Vector responses, Vector acceptances) {
+
+          //  System.out.println("got " + responses.size() + " responses!");
+
+            for(int i=0; i<responses.size(); i++) {
+                ACLMessage msg = ((ACLMessage) responses.get(i)).createReply();
+                msg.setPerformative(ACLMessage.ACCEPT_PROPOSAL); // OR NOT!
+                acceptances.add(msg);
+            }
+        }
+
+        protected void handleAllResultNotifications(Vector resultNotifications) {
+            System.out.println("got " + resultNotifications.size() + " result notifs!");
+        }
+
     }
 }
