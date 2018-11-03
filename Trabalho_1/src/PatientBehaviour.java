8655d396d0aaae0e93f7f3a3f29cde80220497d5
Luís Correia <luis.mcorreia97@gmail.com>
2018-11-03 15:47:44 +0000

    achieveRE patient behaviour

--- Trabalho_1/src/PatientBehaviour.java
+++ Trabalho_1/src/PatientBehaviour.java
@@ -7,22 +7,26 @@ public class PatientBehaviour extends AchieveREInitiator {
     public PatientBehaviour(Agent a, ACLMessage msg) {
         super(a, msg);
     }
+
     protected Vector<ACLMessage> prepareRequests(ACLMessage msg) {
         Vector<ACLMessage> v = new Vector<ACLMessage>();
         // ...
         return v;
     }
+
     protected void handleAgree(ACLMessage agree) {
         // ...
     }
+
     protected void handleRefuse(ACLMessage refuse) {
         // ...
     }
+
     protected void handleInform(ACLMessage inform) {
         // ...
     }
+
     protected void handleFailure(ACLMessage failure) {
         // ...
     }
 }
-
